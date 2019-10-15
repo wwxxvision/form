cd95a3244bb240633afef9cd6722f8e41f64e861
Q-digital <noreply@q-digital.org>
2019-10-15 18:07:53 +0400

    demo build

---
+++ build/precache-manifest.2c2353b50692e6fb019d2f941d93a00b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bb1b19d5e2ab59cfc7a96ba8d137e5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b32699264393e259f3b0",
+    "url": "/static/css/main.d732169a.chunk.css"
+  },
+  {
+    "revision": "1895f770b7e6aa510908",
+    "url": "/static/js/2.80289c6a.chunk.js"
+  },
+  {
+    "revision": "b32699264393e259f3b0",
+    "url": "/static/js/main.c7ca004c.chunk.js"
+  },
+  {
+    "revision": "70d9511d539eeabfd15a",
+    "url": "/static/js/runtime~main.c19376b4.js"
+  },
+  {
+    "revision": "aebd064ec382c9673f3f295d7bffe431",
+    "url": "/static/media/preloader.aebd064e.gif"
+  }
+]);
